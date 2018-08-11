bb7cf3a346d317f7efc760907a5ad3f563daf99c
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-11 13:37:53 +0100

    initial commit

---
+++ src/helpers/request.js
@@ -0,0 +1,12 @@
+const Request = function (url) {
+  this.url = url;
+}
+
+Request.prototype.get = function () {
+  // fetch returns a promise - get data back from url
+  return fetch(this.url)
+  // converts the response text that we get back from the request to a JS object
+  .then(response => response.json());
+}
+
+module.exports = Request;
---
+++ src/models/cities.js
@@ -0,0 +1,2 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
